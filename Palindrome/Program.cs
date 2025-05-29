c455279257ee2df66d0b7052a1c7d7b1dad80536
DinisB <dinisbarroso.dev@gmail.com>
2025-05-29 17:08:10 +0100

    Exercicio 1

---
+++ Palindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool VerifyString(string text)
+            {
+                if (text == null)
+                {
+                    throw new ArgumentNullException(nameof(text));
+                }
+                if (text.Length == 2)
+                {
+                    return true;
+                }
+                char[] charArray = text.ToCharArray();
+                Array.Reverse(charArray);
+                string reversedText = new string(charArray);
+                if (reversedText == text)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            foreach (string arg in args)
+            {
+                bool isPalindrome = VerifyString(arg);
+                Console.WriteLine($"{arg} -> {isPalindrome}");
+            }
+        }
+    }
+}
+
